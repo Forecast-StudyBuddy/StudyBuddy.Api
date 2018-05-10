d70fa3833de0b02edc38662cbf353091aed67778
Aditi Anand <aditianand@pal-nat186-83-200.itap.purdue.edu>
2018-05-10 11:29:07 -0400

    Adding my accepted and requrested calls for request controller

--- api/controllers/requestController.js
+++ api/controllers/requestController.js
@@ -1,4 +1,9 @@
-const { accept_request, create_request, fetch_open_requests, complete_request} = require('../db_calls/request_db_calls')
+const { accept_request, 
+    create_request, 
+    fetch_open_requests, 
+    complete_request, 
+    fetch_my_accepted_requests, 
+    fetch_my_posted_requests} = require('../db_calls/request_db_calls')
 
 // POST
 // recieve a request when a student needs assistance on a course
@@ -18,15 +23,35 @@ exports.request_assistance = function (req, res) {
 // return all open request related to the user
 exports.open_requests = function (req, res) {
     const email = req.body.email
-    fetch_open_requests(email, err => {
+    fetch_open_requests(email, (err, results) => {
         if(!!err) throw err
+        res.send(results)
     })
 }
 
 // GET
 // return an array of my submitted requests and an array of my accepted requests
-exports.my_requests = function (req, res) {
-
+exports.my_accepted_requests = function (req, res) {
+    const email= req.body.email
+    fetch_my_accepted_requests(email, (err, results) => {
+        if(!!err){
+            res.sendStatus(403)
+            console.log(err)
+            return
+        }
+        res.send(results)
+    })
+}
+exports.my_posted_requests = function (req, res) {
+    const email= req.body.email
+    fetch_my_posted_requests(email, (err, results) => {
+        if(!!err){
+            res.sendStatus(403)
+            console.log(err)
+            return
+        }
+        res.send(results)
+    })
 }
 // POST
 // receive an email and request id
