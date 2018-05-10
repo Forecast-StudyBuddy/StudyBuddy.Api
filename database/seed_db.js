dbe4ae05d47bad6eb673bb34c3928df96eb79cfb
darrellshi <shi180@purdue.edu>
2018-05-10 11:54:39 -0400

    seed db refactor

--- database/seed_db.js
+++ database/seed_db.js
@@ -7,44 +7,51 @@ let con = mysql.createConnection({
     database: 'study_buddy_db'
 })
 
-con.connect(function(err) {
-    if(err) throw err
-    console.log('Connected!')
-    insertRequests()
+con.connect(err => {
+    if(!!err) throw err
+
     insertUsers()
-    courseUsers()
-    courses()
-    courseHelper()
+    insertCourses()
+    insertCourseUsers()
+    insertRequests()
 })
 
-function courseHelper(){
-    let insert_course_helpers_sql= 'INSERT INTO course_helpers (course_id, user_id) VALUES ?'
-    var course_helpers_values= [['Math101','abc@purdue.edu'],['CS123','1234@purdue.edu'],['CS101','456@purdue.edu'],['CS114','efg@purdue.edu'],['ENG176','jkl@purdue.edu']]
-    queryDB(insert_course_helpers_sql , course_helpers_values)
+insertUsers = () => {
+    let insert_user_sql= 'INSERT INTO users (name, email, password) VALUES ?'
+    var user_values= [
+        ['Aditi Anand','aditi.anand22@gmail.com','123lala'], 
+        ['Darrell Shi','shi180@purdue.edu','1234'],
+        ['Student 1','test1@purdue.edu','1234'],
+        ['Student 2','test2@purdue.edu','1234'],
+        ['Student 3','test3@purdue.edu','1234'],
+        ['Student 4','test4@purdue.edu','1234'],
+        ['Student 5','test5@purdue.edu','1234'],
+    ]
+    queryDB(insert_user_sql , user_values)
 }
 
-function courses(){
+insertCourses = () => {
     let insert_courses_sql= 'INSERT INTO courses (course_name) VALUES ?'
-    var courses_values= [['Math101'],['CS123'],['CS101'],['CS114'],['ENG176']]
+    var courses_values= [
+        ['MA 16100 - Plane Analytic Geometry And Calculus I'],
+        ['ENGL 10600 - First-Year Composition'],
+        ['CS 18000 - Problem Solving And Object-Oriented Programming'],
+        ['ECON 25200 - Macroeconomics'],
+        ['STAT 35000 - Introduction To Statistics'],
+    ]
     queryDB(insert_courses_sql , courses_values)
 }
 
-function courseUsers(){
+insertCourseUsers = () => {
     let insert_course_users_sql= 'INSERT INTO course_user (course_grade, user_id, course_id) VALUES ?'
     var course_users_values= [['A','aditi.anand22@gmail.com','Math101']]
     queryDB(insert_course_users_sql , course_users_values)
 }
 
-function insertRequests(){
+insertRequests = () => {
     let insert_request_sql= 'INSERT INTO requests (text, user_id, course_id, taker_id, completed) VALUES ?'
     var request_values= [['Need help with Math101','aditi.anand22@gmail.com','Math101', 'sun@purdue.edu', false]]
     queryDB(insert_request_sql , request_values)
-}
-
-function insertUsers(){
-    let insert_user_sql= 'INSERT INTO users (name, email, password) VALUES ?'
-    var user_values= [['Aditi Anand','aditi.anand22@gmail.com','123lala'], ['Sun zang','sun@purdue.edu','pass124']]
-    queryDB(insert_user_sql , user_values)
 }
 
 function queryDB(sql, values) {
