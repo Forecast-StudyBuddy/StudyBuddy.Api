736ceef3454f7a95e6821e06b3c6ee062bd12553
darrellshi <shi180@purdue.edu>
2018-05-10 10:22:58 -0400

    comment out connect and end

--- api/db_calls/course_db_calls.js
+++ api/db_calls/course_db_calls.js
@@ -8,7 +8,7 @@ let con = mysql.createConnection({
 })
 
 exports.fetch_courses_for_user = (email, callback) => {
-    con.connect()
+    // con.connect()
     let fetch_course_ids_sql = `SELECT course_id FROM course_user WHERE user_id=\"${email}\" AND (course_grade=\'A\' OR course_grade=\'B\' OR course_grade IS NULL)`
     let fetch_courses_sql = `SELECT course_name FROM courses WHERE id IN (${fetch_course_ids_sql})`
 
@@ -16,7 +16,7 @@ exports.fetch_courses_for_user = (email, callback) => {
         callback(err, results)
     })
 
-    con.end()
+    // con.end()
 }
 
 exports.insert_helping_courses = (email, courses, callback) => {
@@ -27,12 +27,12 @@ exports.insert_helping_courses = (email, courses, callback) => {
 }
 
 exports.fetch_current_courses = (email, callback) => {
-    con.connect()
+    // con.connect()
     
     let fetch_current_courses_sql = `SELECT course_name FROM courses WHERE id IN (SELECT course_id FROM course_user WHERE user_id=\"${email}\" AND course_grade IS NULL)`
     con.query(fetch_current_courses_sql, (err, results) => {
         callback(err, results)
     })
 
-    con.end()
+    // con.end()
 }
