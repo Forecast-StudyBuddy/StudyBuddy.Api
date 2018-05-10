eb115ad51d86e17f90f98b2d07577dbb256f73ea
Aditi Anand <aditianand@pal-nat186-83-200.itap.purdue.edu>
2018-05-10 13:04:07 -0400

    updating db for new column name changes

--- database/seed_db.js
+++ database/seed_db.js
@@ -14,18 +14,11 @@ con.connect(function(err) {
     insertUsers()
     courseUsers()
     courses()
-    courseHelper()
 })
 
-function courseHelper(){
-    let insert_course_helpers_sql= 'INSERT INTO course_helpers (course_id, user_id) VALUES ?'
-    var course_helpers_values= [['Math101','abc@purdue.edu'],['CS123','1234@purdue.edu'],['CS101','456@purdue.edu'],['CS114','efg@purdue.edu'],['ENG176','jkl@purdue.edu']]
-    queryDB(insert_course_helpers_sql , course_helpers_values)
-}
-
 function courses(){
-    let insert_courses_sql= 'INSERT INTO courses (course_name) VALUES ?'
-    var courses_values= [['Math101'],['CS123'],['CS101'],['CS114'],['ENG176']]
+    let insert_courses_sql= 'INSERT INTO courses (id, course_name) VALUES ?'
+    var courses_values= [['Math101','Introduction to Calculus'],['CS123','Introduction to Computer Science'],['CS101', 'Object Oriented Programming'],['CS114','Operating System'],['ENG176', 'Introduction to English']]
     queryDB(insert_courses_sql , courses_values)
 }
 
@@ -42,7 +35,7 @@ function insertRequests(){
 }
 
 function insertUsers(){
-    let insert_user_sql= 'INSERT INTO users (name, email, password) VALUES ?'
+    let insert_user_sql= 'INSERT INTO users (name, id, password) VALUES ?'
     var user_values= [['Aditi Anand','aditi.anand22@gmail.com','123lala'], ['Sun zang','sun@purdue.edu','pass124']]
     queryDB(insert_user_sql , user_values)
 }
