b0286efa03ffc519bd5530ef40cff43c87df4b15
darrellshi <shi180@purdue.edu>
2018-05-10 11:20:18 -0400

    refactor create database and tables

---
+++ database/setup_databases.js
@@ -0,0 +1,50 @@
+let mysql= require('mysql')
+
+let con = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: 'localmac'
+})
+
+// create database
+con.query("CREATE DATABASE study_buddy_db", function (err, result) {
+    if (!!err) throw err;
+    console.log("Database created");
+
+    con = mysql.createConnection({
+        host:'localhost',
+        user: 'root',
+        password: 'localmac',
+        database: 'study_buddy_db'
+    })
+    
+    // create USERS table
+    let user_table_sql = 'CREATE TABLE users (name VARCHAR(255), id VARCHAR(255) PRIMARY KEY, password VARCHAR(255))'
+    con.query(user_table_sql, (err, result) => {
+        if(!!err) throw err
+        console.log('USERS table created')
+    }) 
+    
+    // create COURSES table
+    let courses_table_sql = 'CREATE TABLE courses (course_name VARCHAR(255), id VARCHAR(255) PRIMARY KEY)'
+    con.query(courses_table_sql, (err, result) => {
+        if(!!err) throw err
+        console.log('COURSES table created')
+    })
+    
+    // create COURSE_USER table
+    let course_user_table_sql = 'CREATE TABLE course_user (course_grade VARCHAR(255), id INT AUTO_INCREMENT PRIMARY KEY, user_id VARCHAR(255), course_id VARCHAR(255) )'
+    con.query(course_user_table_sql, (err, result) => {
+        if(!!err) throw err
+        console.log('COURSE_USER table created')
+    })
+    
+    // create REQUESTS table
+    let request_table_sql = 'CREATE TABLE requests ( text VARCHAR(255), id INT AUTO_INCREMENT PRIMARY KEY, user_id VARCHAR(255), course_id VARCHAR(255), taker_id VARCHAR(255), completed BOOLEAN )'
+    con.query(request_table_sql, (err, result) => {
+        if(!!err) throw err
+        console.log('REQUESTS table created')
+    })
+})
+
+
